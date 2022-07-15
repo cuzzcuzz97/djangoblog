d0f9e9c25b8b3c6936bed1fda4d6419f36ccc421
MacBook Pro <macbookpro@MacBook-Pro-cua-MacBook.local>
2022-07-15 10:41:15 +0700

    nothing

--- blog/settings.py
+++ blog/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'vi-vn'
 
 TIME_ZONE = 'Asia/Ho_Chi_Minh'
 
