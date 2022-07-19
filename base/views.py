e7149f3ee6ce2b648a8e018379bb7b96852f77ed
MacBook Pro <quoccuong.ukh97@gmail.com>
2022-07-19 15:19:35 +0700

    nomore

--- base/views.py
+++ base/views.py
@@ -22,19 +22,19 @@ class BlogDetail(DetailView):
         img = Photo.objects.all()
         return render_to_response("blog.html", {"img": img})
 
-def get_currency(request):
+# def get_currency(request):
     
-    # defining key/request url
-    key = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
+#     # defining key/request url
+#     key = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
     
-    # requesting data from url
+#     # requesting data from url
 
-    data = requests.get(key)  
-    data = data.json()
+#     data = requests.get(key)  
+#     data = data.json()
 
-    def print_price():
-        return data['price']
+#     def print_price():
+#         return data['price']
        
-    context = {'bitcoin' : print_price}
-    time.sleep(10)
-    return render(request , 'base/main.html', context)
+#     context = {'bitcoin' : print_price}
+#     time.sleep(10)
+#     return render(request , 'base/main.html', context)
